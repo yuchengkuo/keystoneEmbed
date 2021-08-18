aacd518502eb8adc1e90bf6925fa8f29813d4ae5
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-08-18 19:32:55 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { withKeystone } = require("@keystone-next/keystone/next");
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = withKeystone();
